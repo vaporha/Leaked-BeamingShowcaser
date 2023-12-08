dc051410241771bbf1a2423f64e74e5c0131b14f
YourWorstNightMare <145917436+vaporha@users.noreply.github.com>
2023-12-08 15:21:13 +0300

    Add files via upload

---
+++ NitroGiftGeneratorAndChecker-master/README.md
@@ -0,0 +1,19 @@
+
+ ⚠️⚠️⚠️   I think we can close this page. Such generators have too little chance of dropping a valid nitro.
+ 
+ ⚠️⚠️⚠️   I opened the topic just for getting to know the github. 
+ 
+ ⚠️⚠️⚠️   The theme will no longer be updated.
+
+ 🔼🔼🔼
+
+
+😎 ℕ𝕚𝕥𝕣𝕠 ℂ𝕠𝕕𝕖 𝔾𝕖𝕟𝕖𝕣𝕒𝕥𝕠𝕣 + 𝕔𝕙𝕖𝕔𝕜𝕖𝕣
+
+😮 3500 gift codes / per 1 minute
+
+
+❓  How use it?
+1. Unzip file
+2. Launch start.py
+3. Wait...
---
+++ NitroGiftGeneratorAndChecker-master/start.py
@@ -0,0 +1,65 @@
+import random
+import string
+import requests
+from threading import Thread
+from time import strftime, gmtime, sleep
+
+print("""
+███╗░░██╗██╗████████╗██████╗░░█████╗░░░░░░░░██████╗░██╗███████╗████████╗
+████╗░██║██║╚══██╔══╝██╔══██╗██╔══██╗░░░░░░██╔════╝░██║██╔════╝╚══██╔══╝
+██╔██╗██║██║░░░██║░░░██████╔╝██║░░██║█████╗██║░░██╗░██║█████╗░░░░░██║░░░
+██║╚████║██║░░░██║░░░██╔══██╗██║░░██║╚════╝██║░░╚██╗██║██╔══╝░░░░░██║░░░
+██║░╚███║██║░░░██║░░░██║░░██║╚█████╔╝░░░░░░╚██████╔╝██║██║░░░░░░░░██║░░░
+╚═╝░░╚══╝╚═╝░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░░░░░░░░╚═════╝░╚═╝╚═╝░░░░░░░░╚═╝░░░    
+░██████╗░███████╗███╗░░██╗███████╗██████╗░░█████╗░████████╗░█████╗░██████╗
+██╔════╝░██╔════╝████╗░██║██╔════╝██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗
+██║░░██╗░█████╗░░██╔██╗██║█████╗░░██████╔╝███████║░░░██║░░░██║░░██║██████╔╝
+██║░░╚██╗██╔══╝░░██║╚████║██╔══╝░░██╔══██╗██╔══██║░░░██║░░░██║░░██║██╔══██╗
+╚██████╔╝███████╗██║░╚███║███████╗██║░░██║██║░░██║░░░██║░░░╚█████╔╝██║░░██║
+░╚═════╝░╚══════╝╚═╝░░╚══╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝
+░░░░░░░░█████╗░██╗░░██╗███████╗░█████╗░██╗░░██╗███████╗██████╗░
+░░██╗░░██╔══██╗██║░░██║██╔════╝██╔══██╗██║░██╔╝██╔════╝██╔══██╗
+██████╗██║░░╚═╝███████║█████╗░░██║░░╚═╝█████═╝░█████╗░░██████╔╝
+╚═██╔═╝██║░░██╗██╔══██║██╔══╝░░██║░░██╗██╔═██╗░██╔══╝░░██╔══██╗
+░░╚═╝░░╚█████╔╝██║░░██║███████╗╚█████╔╝██║░╚██╗███████╗██║░░██║
+░░░░░░░░╚════╝░╚═╝░░╚═╝╚══════╝░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝""")
+sleep(0.3)
+print('https://github.com/Zafros56/NitroGiftGeneratorAndChecker')
+sleep(0.3)
+print('Helped to redo: https://github.com/homka122   \n')
+sleep(0.3)
+
+count_threads = 8
+interval = 5
+
+
+def generate_url():
+    return 'https://discord.gift/' + ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(19)) # At your request
+
+
+def checker(nitro):
+    url = "https://discordapp.com/api/v6/entitlements/gift-codes/" + nitro + "?with_application=false&with_subscription_plan=true"
+    r = requests.get(url)
+    if r.status_code == 200:
+        with open('Nitro Codes Valid.txt', 'a') as f:
+            f.write(nitro + '\n')
+        print(" Valid | {} ".format(nitro))
+
+
+def main():
+    while True:
+        checker(generate_url())
+
+
+def main_info():
+    i = 0
+    while True:
+        checker(generate_url())
+        i += 1
+        if i % interval == 0:
+            print(f'[{strftime("%H:%M:%S", gmtime())}] Checked {i * count_threads} codes')
+
+
+for i in range(count_threads-1):
+    Thread(target=main).start()
+Thread(target=main_info).start()
